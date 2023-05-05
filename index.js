6fd4fa0986ece544025be8616a24cf5381307270
Angel Phillips <bill9889@gmail.com>
2023-05-04 21:22:06 -0600

    Add the D3 modifications for visualization

--- index.js
+++ index.js
@@ -1,75 +1,287 @@
-const width = 1200;
-const height = 900;
+var data_directory = "maps/latin-america.geojson"
+var csv_file = "data-dummy.csv"
+var w = 900;
+var h = 600;
+var w2 = 1350;
+var h2 = 900;
+var padding = 40;
+var spider_ratio = 2.5
+var innerRadius = 350
+var outerRadius = Math.min(w, h)
 
-d3.json("./maps/latin-america.json").then(function (data) {
+function makeTitle(svg, name, subtitle) {
+    svg.append("text")
+        .attr("transform", "translate(" + padding + "," + 25 + ")")
+        .attr("dy", "0em")
+        .attr("font-size", "1.2em")
+        .style("text-anchor", "left")
+        .style("font-family", "sans-serif")
+        .text(name);
 
-    const zoom = d3.zoom()
-	  .scaleExtent([1, 8])
-	  .on("zoom", zoomed);
+    svg.append("text")
+        .attr("transform", "translate(" + padding + "," + 25 + ")")
+        .attr("dy", "1.4em")
+        .attr("font-size", "0.9em")
+        .style("text-anchor", "left")
+        .style("opacity", 0.7)
+        .style("font-family", "sans-serif")
+        .text(subtitle);
+}
 
-    const svg = d3.select('body')
-	  .append('svg')
-	  .attr('width', width)
-	  .attr('height', height)
-	  .on("click", reset);
+function makeAxisTitles(svg, yaxis, xaxis) {
+    svg.append("text")
+        .attr("transform", "translate(" + (padding / 5) + ", " + (h / 2) + ") rotate(-90)")
+        .attr("dy", "1em")
+        .style("font-size", "1.2em")
+        .style("text-anchor", "middle")
+        .style("font-family", "sans-serif")
+        .text(yaxis);
 
-    const projection = d3.geoNaturalEarth1()
-	  .scale(540)
-	  .translate([width / 2, height / 1.4]);
+    svg.append("text")
+        .attr("transform", "translate(" + (w / 2) + "," + (h - padding/2) + ")")
+        .attr("dy", "1em")
+        .attr("class", "x-axis-title")
+        .attr("font-size", "1.2em")
+        .style("text-anchor", "middle")
+        .style("font-family", "sans-serif")
+        .text(xaxis);
+}
 
-    const path = d3.geoPath(projection);
+// Create the color function, and generate data for legends
+colorlegend = [20e3, 40e3, 60e3, 80e3, 100e3]
+var color = d3.scaleSequential(d3.interpolateViridis)
+    .domain([0, 120000]);
 
-    const g = svg.append("g");
+function makeXscale(data, name) {
+    var min = d3.min(data.features, function(d) { return d.properties[name]; })
+    var max = d3.max(data.features, function(d) { return d.properties[name]; })
+    var xScale = d3.scaleLinear()
+        .domain([min, max])
+        .range([0 + padding + 40, w - padding])
 
-    const countries = g.append('g')
-	  .attr("fill", "#444")
-	  .attr("cursor", "pointer")
-          .selectAll("path")
-	  .data(topojson.feature(data, data.objects.countries).features)
-	  .enter()
-	  .append("path")
-	  .on("click", clicked)
-	  .attr("d", path);
+    return xScale
+}
 
-    countries.append("title")
-	.text(d => d.properties.name);
+// Function to make legend for chloropleth
+function makeLegend(svg, data, colorfunc, yoffset = (h / 2) - 100) {
+    // Create legend
+    var legend = svg.selectAll("g")
+        .data(data)
+        .enter()
+        .append("g")
+        .attr("transform", function(d, i) { return "translate(" + -100 + "," + (yoffset + padding + (i * 20)) + ")" })
 
-    g.append("path")
-	.attr("fill", "none")
-	.attr("stroke", "white")
-	.attr("stroke-linejoin", "round")
-	.attr("d", path(topojson.mesh(data, data.objects.countries, (a, b) => a !== b)));
+    legend.append("rect")
+        .attr("class", "legend-rect")
+        .attr("x", w - 150)
+        .attr("width", 19)
+        .attr("height", 19)
+        .attr("fill", function(d) { return color(d) })
 
-    svg.call(zoom)
+    legend.append("text")
+        .attr("class", "legend-text")
+        .attr("x", w - 100)
+        .attr("font-family", "sans-serif")
+        .attr("y", 9.5)
+        .attr("dy", "0.4em")
+        .attr("font-size", "0.8em")
+        .text(function(d) { return formatAsThousands(d); })
+
+    return legend
+}
+
+var formatAsThousands = d3.format(","); //e.g. converts 123456 to "123,456"
+
+//Create SVG element
+var svg = d3.select("body").select("#Plot1")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .attr("transform", "translate(300,150)");
+
+    svg2 = d3.select("body").select("#Plot2")
+    .append("svg")
+    .attr("width", w2)
+    .attr("height", h2)
+    .style("font-family", "sans-serif")
     
-    function reset() {
-	countries.transition().style("fill", null);
-	svg.transition().duration(750).call(
-	    zoom.transform,
-	    d3.zoomIdentity,
-	    d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
-	);
-    }
+d3.json(data_directory).then( function(json) {
 
-    function clicked(event, d) {
-	const [[x0, y0], [x1, y1]] = path.bounds(d);
-	event.stopPropagation();
-	countries.transition().style("fill", null);
-	d3.select(this).transition().style("fill", "red");
-	svg.transition().duration(750).call(
-	    zoom.transform,
-	    d3.zoomIdentity
-		.translate(width / 2, height / 2)
-		.scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
-		.translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
-	    d3.pointer(event, svg.node())
-	);
-    }
+    var projection = d3.geoNaturalEarth1()
+        .fitSize([w - 50, h - 50], json)
 
-    function zoomed(event, d) {
-	g.attr("transform", event.transform);
-	g.attr("stroke-width", 1 / event.transform.k);
-    }               
+    var path = d3.geoPath(projection);
+
+    //Bind data and create one path per GeoJSON feature
+    var geos = svg.selectAll("path")
+        .data(json.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("transform", "translate(-50,50)")
+        .style("fill", function(d) {
+            //Get data value
+            var value = 60000;//d.properties.income;
+            if (value) {
+                return color(value);
+            } else {
+                return "#ccc";
+            }
+        });
+
+    geos.on("mouseover", function(d) {
+        d3.select(this)
+            .transition()
+            .duration(150)
+            .style("fill", "orange")
+
+        var xPosition = 250;
+        var yPosition = 250;
+
+        d3.select("#tooltip")
+                .style("left", (d.pageX) + "px")   
+                .style("top", (d.pageY - 30) + "px")
+                .select("#value")
+                .text(d.currentTarget.__data__.properties.name)
+
+        d3.select("#tooltip").classed("hidden", false)
+
+        // var update_data = calc_data(d.properties)
+        // update_poly = []
+        // update_data.forEach(function(d, i) { 
+        //     x = (radius * d) * (1 - Math.sin(i * (2*Math.PI) / total))
+        //     x += (h/2 + padding*spider_ratio - (radius *d))
+        //     y = (radius * d) * (1 - Math.cos(i * (2*Math.PI) / total))
+        //     y += (h/2 + padding - (radius * d))
+        //     update_poly.push({ 'x' : x, 'y': y})
+        // })
+        // svg2.selectAll("circle")
+        //     .data(update_data)
+        //     .transition()
+        //     .duration(200)
+        //     .attr("cx", function(l, i) { return ( (radius * l) * (1 - Math.sin(i * radians / total)) )})
+        //     .attr("cy", function(l, i) { return ( (radius * l) * (1 - Math.cos(i * radians / total)) )})
+        //     .attr("r", 5)
+        //     .attr("transform", function(l) { return ("translate(" + (h/2 + padding*spider_ratio - (radius * l)) + "," + (h/2 + padding - (radius * l)) + ")") })
+        //     .attr("fill", "black")
+        //     .attr("opacity", "0.8")
+
+        // svg2.selectAll("polygon")
+        //     .data([update_poly])
+        //     .transition()
+        //     .duration(200)
+        //     .style("stroke", "black")
+        //     .style('fill', "gray")
+        //     .attr("points",function(l) { 
+        //         return l.map(function(l) {
+        //             return [l.x, l.y].join(",");
+        //         }).join(" ")
+        //     })
+        //     .attr("opacity", 0.5)
+    })
+
+    geos.on("mouseout", function(d) {
+        d3.select(this)
+            .transition()
+            .duration(300)
+            .style("fill", function(d) {
+                //Get data value
+                var value = 40000; //d.properties.income;
+                if (value) {
+                    //If value exists…
+                    return color(value);
+                } else {
+                    //If value is undefined…
+                    return "#ccc";
+                }
+            })
+
+        d3.select("#tooltip").classed("hidden", true)
+    })
+
+});
+
+d3.csv(csv_file).then( function(data) {
+  
+    var band = d3.scaleBand()
+        .domain(data.map(d => d.State))
+        .range([0, 2 * Math.PI])
+        .align(0)
+
+    var radial = d3.scaleRadial()
+        .domain([0, d3.max(data,(d, _, data) => {
+            let total = 0;
+            for (let i = 1; i < data.columns.length; ++i) total += d[data.columns[i]] = +d[data.columns[i]];
+            return total;  
+            })])
+        .range([innerRadius, outerRadius])
+
+    var ordinal =d3.scaleSequential(d3.interpolateBlues)
+        .domain([0, d3.max(data,(d, _, data) => {
+            let total = 0;
+            for (let i = 1; i < data.columns.length; ++i) total += d[data.columns[i]] = +d[data.columns[i]];
+            return total;  
+            })])
+
+    var arc = d3.arc()
+        .innerRadius(d => radial(d[0]))
+        .outerRadius(d => radial(d[1]))
+        .startAngle(d => band(d.data.State))
+        .endAngle(d => band(d.data.State) + band.bandwidth())
+        .padAngle(0.01)
+        .padRadius(innerRadius)
+        
+
+    svg2.append("g")
+        .selectAll("g")
+        .data(d3.stack().keys(data.columns.slice(1))(data))
+        .join("g")
+        .selectAll("path")
+        .data(d => d)
+        .join("path")
+        .attr("fill", d => ordinal(d.key))
+        .attr("d", arc)
+        .attr("transform", "translate(" + ((w2) / 2) + "," + ((h2) / 2) + ")");
+
+    var g = svg2.append("g")
+                .attr("text-anchor", "middle")
+                .attr("transform", "translate(" + ((w2) / 2) + "," + ((h2) / 2) + ")")
+                .selectAll("g")
+                .data(data)
+                .join("g")
+                .attr("transform", d => `                                                                                                                                                                           
+                    rotate(${((band(d.State) + band.bandwidth() / 2) * 180 / Math.PI - 90)})
+                    translate(${innerRadius},0)
+                `)
     
+    g.append("line")
+        .attr("x2", -5)
+        .attr("stroke", "#000")
+
+    g.append("text")
+        .attr("transform", d => (band(d.State) + band.bandwidth() / 2 + Math.PI / 2) % (2 * Math.PI) < Math.PI
+            ? "rotate(90)translate(0,16)"
+            : "rotate(-90)translate(0,-9)")
+        .text(d => d.State)
+    
+
+    var g = svg2.append("g")
+                .attr("text-anchor", "middle")
+    
+    g.append("text")
+                .attr("y", d => -radial(radial.ticks(5).pop()))
+                .attr("dy", "-1em")
+                .text("Population")
+                .selectAll("g")
+                .data(radial.ticks(5).slice(1))
+                .join("g")
+                .attr("fill", "none")
+                .attr("transform", "translate(" + ((w2) / 2) + "," + ((h2) / 2) + ")");
+
+    g.append("circle")
+                .attr("stroke", "#000")
+                .attr("stroke-opacity", 0.5)
+                .attr("r", radial)
+                .attr("transform", "translate(" + ((w2) / 2) + "," + ((h2) / 2) + ")");
 });
 
