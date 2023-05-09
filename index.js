c179e8fdf2a875494c412a40ef33304663d86b51
jimmydragneel14 <jimmymokz14@gmail.com>
2023-05-08 20:53:45 -0600

    Update visualization based on map and dropdown

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 //Data
 var data_directory = "maps/latin-america.geojson"
 var csv_file = "data-dummy.csv"
-var csv_access = "./csv_assets/energy_access.csv"
-var csv_energy_consumption = "./csv_assets/energy_consumption.csv"
-var csv_inflation = "./csv_assets/inflation.csv"
-var csv_enrollment = "./csv_assets/school_enrollment.csv"
+var csv_access = "csv_assets/energy_access.csv"
+var csv_energy_consumption = "csv_assets/energy_consumption.csv"
+var csv_inflation = "csv_assets/inflation.csv"
+var csv_enrollment = "csv_assets/school_enrollment.csv"
 
 //global
 var current_country = ""
-var current_data = ""
+var current_csv = csv_inflation
+var consumption_data = ""
 
 //Specification
 var w = 900;
 var h = 600;
 var w2 = 1350;
-var h2 = 900;
+var h2 = 1200;
 var padding = 40;
 var spider_ratio = 2.5
-var innerRadius = 350
+var innerRadius = 450
 var outerRadius = Math.min(w, h)
+
+//Other data
+columns = ["Year", "Data"]
+energy_access_description = "This data represents the percentage of the population of a country that has access to electricity"
+inflation_description = "This data corresponds to the percentage of inflation, which is measured by the annual growth rate of the implicit GDP deflator"
+enrollment_description = "This data corresponds to the net secondary enrollment rate"
+
+document.getElementById("country").innerHTML = "N/A";
+document.getElementById("secondary_variable").innerHTML = "Inflation";
+document.getElementById("secondary_description").innerHTML = inflation_description;
+
 
 function findCountryData(country, dataset){
     for (let i=0; i < dataset.length; i++) {
@@ -28,7 +40,15 @@ function findCountryData(country, dataset){
     }
 }
 
+function checkRange(num, min, max) {
+    return num >= min && num <= max;
+  }
+  
 
+
+d3.csv(csv_energy_consumption).then( function(data) {
+    consumption_data = data;
+})
 
 function makeTitle(svg, name, subtitle) {
     svg.append("text")
@@ -173,13 +193,15 @@ d3.json(data_directory).then( function(json) {
             d3.select("#tooltip").classed("hidden", false)
 
             current_country =  d.currentTarget.__data__.properties.name
-            var country_data_parsed = [{"columns": ["Year", "Data"]}]
+            var country_data_parsed = []
 
-            d3.csv(csv_access).then( function(data){
+            d3.csv(current_csv).then( function(data){
                 var country_data_raw = findCountryData(current_country, data)
                 Object.keys(country_data_raw).map((key) => country_data_parsed.push({"Year": key, "Data": parseFloat(country_data_raw[key])}));
-                current_data = country_data_parsed
 
+                changeCountry(country_data_parsed)
+                document.getElementById("country").innerHTML = current_country;
+                
             })
 
           
@@ -246,20 +268,15 @@ d3.json(data_directory).then( function(json) {
 
 });
 
-d3.csv(csv_access).then( function(xdata) {
-    delete xdata["columns"]
-    console.log(xdata)
+d3.csv(csv_inflation).then( function(xdata) {
 
-
-    var country_data_parsed = [{"columns": ["Year", "Data"]}]
-    var country_data_raw = findCountryData("Brazil", xdata)
-    console.log(country_data_raw)
+    var country_data_parsed = []
+    var country_data_raw = findCountryData("Default", xdata)
     delete country_data_raw["country_name"]
 
     Object.keys(country_data_raw).map((key) => country_data_parsed.push({"Year": key, "Data": parseFloat(country_data_raw[key])}));
+
     var data = country_data_parsed
-
-
 
     var band = d3.scaleBand()
         .domain(data.map(d => d.Year))
@@ -270,7 +287,7 @@ d3.csv(csv_access).then( function(xdata) {
     var radial = d3.scaleRadial()
         .domain([0, d3.max(data,(d, _, data) => {
             let total = 0;
-            for (let i = 1; i < data[0].columns.length; ++i) total += d[data[0].columns[i]] = +d[data[0].columns[i]];
+            for (let i = 1; i < columns.length; ++i) total += d[columns[i]] = +d[columns[i]];
             return total;  
             })])
         .range([innerRadius, outerRadius])
@@ -278,7 +295,7 @@ d3.csv(csv_access).then( function(xdata) {
     var ordinal =d3.scaleSequential(d3.interpolateBlues)
         .domain([0, d3.max(data,(d, _, data) => {
             let total = 0;
-            for (let i = 1; i < data[0].columns.length; ++i) total += d[data[0].columns[i]] = +d[data[0].columns[i]];
+            for (let i = 1; i < columns.length; ++i) total += d[columns[i]] = +d[columns[i]];
             return total;  
             })])
 
@@ -289,18 +306,28 @@ d3.csv(csv_access).then( function(xdata) {
         .endAngle(d => band(d.data.Year) + band.bandwidth())
         .padAngle(0.01)
         .padRadius(innerRadius)
-        
+
 
     svg2.append("g")
         .selectAll("g")
-        .data(d3.stack().keys(data[0].columns.slice(1))(data))
+        .data(d3.stack().keys(columns.slice(1))(data))
         .join("g")
         .selectAll("path")
         .data(d => d)
         .join("path")
         .attr("fill", d => ordinal(d.key))
         .attr("d", arc)
-        .attr("transform", "translate(" + ((w2) / 2) + "," + ((h2) / 2) + ")");
+        .attr("transform", "translate(" + ((w2) / 2) + "," + ((h2) / 2) + ")")
+        .style("fill", function(d) {
+            //Get data value
+            var value = 60000;//d.properties.income;
+            var country_consumption_data = findCountryData(current_country,consumption_data)
+            delete country_data_raw["country_name"]
+            if (value) {
+                return color(value);
+            } else {
+                return "#ccc";
+            }});
 
     var g = svg2.append("g")
                 .attr("text-anchor", "middle")
@@ -343,3 +370,151 @@ d3.csv(csv_access).then( function(xdata) {
                 .attr("r", radial)
                 .attr("transform", "translate(" + ((w2) / 2) + "," + ((h2) / 2) + ")");
 });
+
+function changeCountry(data) {
+    data.pop()
+    updateSVG (data)
+}
+
+
+
+function changeTheme() {
+    var country_data_parsed = []
+    select_value = document.getElementById('format').value
+    document.getElementById("secondary_variable").innerHTML = select_value;
+
+    if (select_value === "Energy Access"){
+        current_csv = csv_access;
+        document.getElementById("secondary_description").innerHTML = energy_access_description;
+    } else if (select_value === "Inflation"){
+        current_csv = csv_inflation;
+        document.getElementById("secondary_description").innerHTML = inflation_description;
+    } else if (select_value === "Secondary Enrollment"){
+        current_csv = csv_enrollment;
+        document.getElementById("secondary_description").innerHTML = enrollment_description;
+    } else {
+        document.getElementById("secondary_description").innerHTML = "";
+    }
+
+    var country_data_parsed = []
+    d3.csv(current_csv).then( function(data){
+        if (current_country === ""){
+            current_country = "Default"
+        }
+
+        var country_data_raw = findCountryData(current_country, data)
+        delete country_data_raw["country_name"]
+        Object.keys(country_data_raw).map((key) => country_data_parsed.push({"Year": key, "Data": parseFloat(country_data_raw[key])}));
+        updateSVG(country_data_parsed)
+        })
+}
+
+function updateSVG (data){
+    d3.selectAll("g").remove()
+
+    var band = d3.scaleBand()
+        .domain(data.map(d => d.Year))
+        .range([0, 2 * Math.PI])
+        .align(0)
+
+
+    var radial = d3.scaleRadial()
+        .domain([0, d3.max(data,(d, _, data) => {
+            let total = 0;
+            for (let i = 1; i < columns.length; ++i) total += d[columns[i]] = +d[columns[i]];
+            return total;  
+            })])
+        .range([innerRadius, outerRadius])
+
+    var ordinal =d3.scaleSequential(d3.interpolateBlues)
+        .domain([0, d3.max(data,(d, _, data) => {
+            let total = 0;
+            for (let i = 1; i < columns.length; ++i) total += d[columns[i]] = +d[columns[i]];
+            return total;  
+            })])
+
+    var arc = d3.arc()
+        .innerRadius(d => radial(d[0]))
+        .outerRadius(d => radial(d[1]))
+        .startAngle(d => band(d.data.Year))
+        .endAngle(d => band(d.data.Year) + band.bandwidth())
+        .padAngle(0.01)
+        .padRadius(innerRadius)
+
+
+    svg2.append("g")
+        .selectAll("g")
+        .data(d3.stack().keys(columns.slice(1))(data))
+        .join("g")
+        .selectAll("path")
+        .data(d => d)
+        .join("path")
+        .attr("fill", d => ordinal(d.key))
+        .attr("d", arc)
+        .attr("transform", "translate(" + ((w2) / 2) + "," + ((h2) / 2) + ")")
+        .style("fill", function(d) {
+            //MAX 3895,436156
+            //MIN 18,96851107
+            var value = 60000;//d.properties.income;
+            if (current_country === ""){
+                current_country ="Default"
+            }
+            var country_consumption_data = findCountryData(current_country,consumption_data)
+
+            // console.log(country_consumption_data[d.data["Year"]])
+            // console.log(d.data["Year"])
+            // console.log(d.data["Data"])
+            if (checkRange(country_consumption_data[d.data["Year"]], 0, 999)) {
+                return "#FFB888";
+            } else if (checkRange(country_consumption_data[d.data["Year"]], 1000, 1999)) {
+                return "#FF8C3F";
+            } else if (checkRange(country_consumption_data[d.data["Year"]], 2000, 2999)) {
+                return "#E45C00";
+            } else if (checkRange(country_consumption_data[d.data["Year"]], 3000, 3999)) {
+                return "#C0362C";
+            }
+        });
+
+    var g = svg2.append("g")
+                .attr("text-anchor", "middle")
+                .attr("transform", "translate(" + ((w2) / 2) + "," + ((h2) / 2) + ")")
+                .selectAll("g")
+                .data(data)
+                .join("g")
+                .attr("transform", d => `                                                                                                                                                                           
+                    rotate(${((band(d.Year) + band.bandwidth() / 2) * 180 / Math.PI - 90)})
+                    translate(${innerRadius},0)
+                `)
+    
+    g.append("line")
+        .attr("x2", -5)
+        .attr("stroke", "#000")
+
+    g.append("text")
+        .attr("transform", d => (band(d.Year) + band.bandwidth() / 2 + Math.PI / 2) % (2 * Math.PI) < Math.PI
+            ? "rotate(90)translate(0,16)"
+            : "rotate(-90)translate(0,-9)")
+        .text(d => d.Year)
+    
+
+    var g = svg2.append("g")
+                .attr("text-anchor", "middle")
+    
+    g.append("text")
+                .attr("y", d => -radial(radial.ticks(5).pop()))
+                .attr("dy", "-1em")
+                .text("Population")
+                .selectAll("g")
+                .data(radial.ticks(5).slice(1))
+                .join("g")
+                .attr("fill", "none")
+                .attr("transform", "translate(" + ((w2) / 2) + "," + ((h2) / 2) + ")");
+
+    g.append("circle")
+                .attr("stroke", "#000")
+                .attr("stroke-opacity", 0.5)
+                .attr("r", radial)
+                .attr("transform", "translate(" + ((w2) / 2) + "," + ((h2) / 2) + ")");
+}
+
+
